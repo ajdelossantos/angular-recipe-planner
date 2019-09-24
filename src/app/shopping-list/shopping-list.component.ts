ffcd1b6935bbb605ecb7027963eedef553d4c611
Alvin James T. Delos Santos <alvin.delossantos@rstor.io>
2019-09-24 13:50:17 -0700

    feat: implement shopping components

--- src/app/shopping-list/shopping-list.component.ts
+++ src/app/shopping-list/shopping-list.component.ts
@@ -1,12 +1,17 @@
 import { Component, OnInit } from '@angular/core';
 
+import { Ingredient } from '../shared/ingredient.model';
+
 @Component({
   selector: 'app-shopping-list',
   templateUrl: './shopping-list.component.html',
   styleUrls: ['./shopping-list.component.css']
 })
 export class ShoppingListComponent implements OnInit {
-  ingredients = [];
+  ingredients: Ingredient[] = [
+    new Ingredient('ground beef', 1),
+    new Ingredient('tomatoes', 4),
+  ];
 
   constructor() { }
 
