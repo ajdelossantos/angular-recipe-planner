f3011db62ed7bec15366a56cfaabf913f3e5e26e
Alvin James T. Delos Santos <alvin.delossantos@rstor.io>
2019-09-23 17:37:16 -0700

    fix: move recipe list markup to correct component

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,11 +1,25 @@
 import { Component, OnInit } from '@angular/core';
 
+import { Recipe } from '../recipe.model';
+
 @Component({
   selector: 'app-recipe-list',
   templateUrl: './recipe-list.component.html',
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
+  recipes: Recipe[] = [
+    new Recipe(
+      'pepperoni pizza',
+      'a weeknight classic',
+      'https://image.shutterstock.com/z/stock-photo-pizza-margherita-230090839.jpg'
+    ),
+    new Recipe(
+      'cheese pizza',
+      'a weeknight vegetarian classic',
+      'https://image.shutterstock.com/z/stock-photo-pizza-margherita-230090839.jpg'
+    )
+  ];
 
   constructor() { }
 
