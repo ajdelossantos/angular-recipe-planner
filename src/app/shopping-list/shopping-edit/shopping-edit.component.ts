ea0335788c2e53f36446e510e4f313a2e1d7b537
Alvin James T. Delos Santos <alvin.delossantos@rstor.io>
2019-10-03 16:41:04 -0700

    feat: add forms module, impl add shopping item

--- src/app/shopping-list/shopping-edit/shopping-edit.component.ts
+++ src/app/shopping-list/shopping-edit/shopping-edit.component.ts
@@ -1,4 +1,13 @@
-import { Component, OnInit } from '@angular/core';
+import {
+  Component,
+  OnInit,
+  ElementRef,
+  ViewChild,
+  EventEmitter,
+  Output
+} from '@angular/core';
+
+import { Ingredient } from 'src/app/shared/ingredient.model';
 
 @Component({
   selector: 'app-shopping-edit',
@@ -6,10 +15,21 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./shopping-edit.component.css']
 })
 export class ShoppingEditComponent implements OnInit {
+  @ViewChild('nameInput', { static: false }) nameInputref: ElementRef;
+  @ViewChild('amountInput', { static: false }) amountInputref: ElementRef;
+  @Output() ingredientAdded = new EventEmitter<Ingredient>();
 
   constructor() { }
 
   ngOnInit() {
+  }
+
+  onAddItem() {
+    const iName = this.nameInputref.nativeElement.value;
+    const iAmount = this.amountInputref.nativeElement.value;
+
+    const newIngredient = new Ingredient(iName, iAmount)
+    this.ingredientAdded.emit(newIngredient);
   }
 
 }
